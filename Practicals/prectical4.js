11413117fac30c32323e904cad25730ecb6e059d
ZeroMeet <110345548+ZeroMeet@users.noreply.github.com>
2023-07-26 10:58:29 +0530

    Update prectical4.js

--- Practicals/prectical4.js
+++ Practicals/prectical4.js
@@ -12,4 +12,14 @@ function factorial(num) {
   
   const num = 5;
   console.log(`Factorial of ${num} is: ${factorial(num)}`);
+
+let global = "global variable";
+
+function globallocal() {
+  let local = "local variable";
+
+  console.log("Accessing global variable:", global);
+  console.log("Accessing local variable:", local);
+}
+globallocal()
   
